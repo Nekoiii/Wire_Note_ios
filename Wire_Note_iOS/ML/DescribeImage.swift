734ed943cc531cc73a0792619e5f56a50794ac50
Nekoiii <10nekosa@gmail.com>
2024-05-27 22:47:32 +0900

    use describe api in swift. be able to describe a test img in DescribeImageView now.;

---
+++ Wire_Note_iOS/ML/DescribeImage.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+func describeImage(imageData: Data, completion: @escaping (Result<String, Error>) -> Void) {
+    guard let url = URL(string: "http://127.0.0.1:5001/describe") else {
+        print("Invalid URL")
+        return
+    }
+
+    let boundary = UUID().uuidString
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+
+    var body = Data()
+    body.append("--\(boundary)\r\n".data(using: .utf8)!)
+    body.append("Content-Disposition: form-data; name=\"file\"; filename=\"image.jpg\"\r\n".data(using: .utf8)!)
+    body.append("Content-Type: image/jpeg\r\n\r\n".data(using: .utf8)!)
+    body.append(imageData)
+    body.append("\r\n--\(boundary)--\r\n".data(using: .utf8)!)
+
+    request.httpBody = body
+
+    URLSession.shared.dataTask(with: request) { data, response, error in
+        if let error = error {
+            completion(.failure(error))
+            return
+        }
+        
+        guard let data = data else {
+            print("No data")
+            completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "No data received"])))
+            return
+        }
+        
+        do {
+            let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            if let description = json?["description"] as? String {
+                completion(.success(description))
+            } else {
+                completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "No description found"])))
+            }
+        } catch let jsonError {
+            completion(.failure(jsonError))
+        }
+    }.resume()
+}
---
+++ Wire_Note_iOS/Views/DescribeImageView.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+
+struct DescribeImageView: View {
+    @State private var description: String = "Loading..."
+    @State private var errorMessage: String?
+    
+    var body: some View {
+        VStack {
+            if let errorMessage = errorMessage {
+                Text("Error: \(errorMessage)")
+                    .foregroundColor(.red)
+            } else {
+                Text(description)
+                    .padding()
+            }
+            Button(action: loadImageAndDescribe) {
+                Text("Describe Image")
+            }
+        }
+        .onAppear(perform: loadImageAndDescribe)
+    }
+    
+    private func loadImageAndDescribe() {
+        if let image = UIImage(named: "icon-1"), let imageData = image.pngData() {
+            describeImage(imageData: imageData) { result in
+                switch result {
+                case .success(let description):
+                    DispatchQueue.main.async {
+                        self.description = description
+                    }
+                case .failure(let error):
+                    DispatchQueue.main.async {
+                        self.errorMessage = error.localizedDescription
+                    }
+                }
+            }
+        } else {
+            errorMessage = "Failed to load image from assets."
+        }
+    }
+}
+
+struct DescribeImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        DescribeImageView()
+    }
+}
